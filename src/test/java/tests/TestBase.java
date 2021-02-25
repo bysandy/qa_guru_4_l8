4d05a067810b0049d5a7e3992db48b56932f418e
Bysandy <leeonid@gmil.com>
2021-02-26 00:22:12 +0300

    updated TestBase

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -22,7 +22,8 @@ public class TestBase {
             capabilities.setCapability("enableVNC", true);
             capabilities.setCapability("enableVideo", true);
             Configuration.browserCapabilities = capabilities;
-            Configuration.remote = System.getProperty("remote_driver");
+//            Configuration.remote = System.getProperty("remote_driver");
+            Configuration.remote = "https://user1:1234@selenoid.autotests.cloud:4444/wd/hub/";
         }
     }
 //    @AfterEach
