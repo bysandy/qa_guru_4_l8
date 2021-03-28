0e0d04e5fe11bf9562583e4587b4d37116fb946a
Bysandy <leeonid@gmil.com>
2021-03-28 10:42:27 +0300

    Update TestBase.java remove curl brackets

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -32,9 +32,8 @@ public class TestBase {
         attachScreenshot("Last screenshot");
         attachPageSource();
         attachAsText("Browser console logs", getConsoleLogs());
-        if(System.getProperty("video_storage") != null) {
+        if(System.getProperty("video_storage") != null)
             attachVideo();
-        }
         closeWebDriver();
     }
 }
